755d55d16d39cfad0c1e1cd32c67e603a6abe5e0
Zhu,Shenghang S <s.zhu@student.fontys.nl>
2021-03-03 13:43:53 +0100

    install react

--- src/main/java/nl/getandgo/springboot/Controller/ProductController.java
+++ src/main/java/nl/getandgo/springboot/Controller/ProductController.java
@@ -5,19 +5,21 @@ import nl.getandgo.springboot.FakeDataStore;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("products")
 public class ProductController {
 
     private static final FakeDataStore Data=new FakeDataStore();
 
     //Get All Products
-    @GetMapping("/products")
-    public List<Product> getProducts() {
+    @GetMapping
+    public List<Product> getAllProducts() {
         return Data.getProducts();
     }
 
