2ad254819ffcfa226254e873e9017cb950c177d1
Zhu,Shenghang S <s.zhu@student.fontys.nl>
2021-03-31 19:27:01 +0200

    1.Add @CORS Configuration

--- src/main/java/nl/getandgo/application/Application.java
+++ src/main/java/nl/getandgo/application/Application.java
@@ -2,18 +2,21 @@ package nl.getandgo.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-// is a convenience annotation that adds all of the following:
-// - @Configuration : Tags the class as source of bean definitions for the application context.
-// - @EnableAutoConfiguration : Tells Spring Boot to start adding beans based on classpath setting,
-//                              other beans, and various property setting.For example,
-//                              if "spring-webmvc" is on the class path, this annotation flags
-//                              the application as a web application and activates key behaviors,
-//                              such as setting up a "DispatcherServlet".
-// - @ComponentScan : Tells Spring to look for other components, configurations, and services in
-//                    the "com/example" package, letting it find the controllers.
 public class Application {
     public static void main(String[] args) { SpringApplication.run(Application.class, args); }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("*").allowedOrigins("http://localhost:8080");
+            }
+        };
+    }
  }
 
--- src/main/java/nl/getandgo/application/store/StoreController.java
+++ src/main/java/nl/getandgo/application/store/StoreController.java
@@ -1,14 +1,12 @@
 package nl.getandgo.application.store;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class StoreController {
     private final StoreService storeService;
     @Autowired
